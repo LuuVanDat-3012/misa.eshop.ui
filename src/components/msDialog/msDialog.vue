07fe4bcc1f67e66b7548c0263400c33600f907d9
Đạt Lưu <lovanmet0512@gmail.com>
2021-06-14 17:21:17 +0700

    done dialog popup

--- src/components/msDialog/msDialog.vue
+++ src/components/msDialog/msDialog.vue
@@ -4,81 +4,152 @@
     <div class="ms-dialog-main">
       <div class="ms-dialog-header">
         <div class="ms-header-title">Thêm mới cửa hàng</div>
-        <div class="ms-icon-close"></div>
+        <div class="ms-icon-close" @click="closeDialog"></div>
       </div>
 
       <div class="ms-dialog-body">
         <div class="ms-row">
           <div class="ms-row-text">
-            Mã cửa hàng <span class="ms-field-requied">(*)</span>
+            Mã cửa hàng <span class="ms-field-requied">*</span>
           </div>
           <div class="ms-form-text">
-              <input type="text" class="ms-field-input" />
+            <input type="text" class="ms-field-input" v-model="store.storeCode"/>
           </div>
         </div>
 
         <div class="ms-row">
           <div class="ms-row-text">
-            Tên cửa hàng <span class="ms-field-requied">(*)</span>
+            Tên cửa hàng <span class="ms-field-requied" >*</span>
           </div>
           <div class="ms-form-text">
-               <input type="text" class="ms-field-input" />
+            <input type="text" class="ms-field-input" v-model="store.storeName"/>
           </div>
         </div>
 
         <div class="ms-row-large">
           <div class="ms-row-text">
-            Địa chỉ<span class="ms-field-requied">(*)</span>
+            Địa chỉ<span class="ms-field-requied">*</span>
           </div>
-          <div class="ms-form-text">
-               <input type="text" class="ms-field-input" />
+          <div class="ms-form-textarea">
+            <input type="textarea" class="ms-field-textarea"  v-model="store.address"/>
           </div>
         </div>
 
         <div class="ms-row ms-row-2-col">
           <div class="ms-col">
             <div class="ms-row-text">Số điện thoại</div>
-            <div class="ms-form-text"></div>
+            <div class="ms-form-text">
+               <input type="text" class="ms-field-input" v-model="store.phoneNumber"/>
+            </div>
           </div>
           <div class="ms-col">
-            <div class="ms-row-text">Mã số thuế</div>
-            <div class="ms-form-text"></div>
+            <div class="ms-row-text text-padding-10px">Mã số thuế</div>
+            <div class="ms-form-text">
+               <input type="text" class="ms-field-input" v-model="store.storeTaxCode"/>
+            </div>
           </div>
         </div>
 
         <div class="ms-row">
           <div class="ms-row-text">Quốc gia</div>
-          <div class="ms-form-text"></div>
+          <div class="ms-form-select align-item-left">
+            <ms-combobox class="custom-combobox" />
+          </div>
         </div>
 
         <div class="ms-row ms-row-2-col">
           <div class="ms-col">
             <div class="ms-row-text">Tỉnh/Thành phố</div>
-            <div class="ms-form-text"></div>
+            <div class="ms-form-select">
+              <ms-combobox class="custom-combobox" />
+            </div>
           </div>
           <div class="ms-col">
-            <div class="ms-row-text">Quận/Huyện</div>
-            <div class="ms-form-text"></div>
+            <div class="ms-row-text text-padding-10px">Quận/Huyện</div>
+            <div class="ms-form-select">
+              <ms-combobox class="custom-combobox" />
+            </div>
           </div>
         </div>
 
         <div class="ms-row ms-row-2-col">
           <div class="ms-col">
             <div class="ms-row-text">Phường/Xã</div>
-            <div class="ms-form-text"></div>
+            <div class="ms-form-select">
+              <ms-combobox class="custom-combobox" />
+            </div>
           </div>
           <div class="ms-col">
-            <div class="ms-row-text">Đường phố</div>
+            <div class="ms-row-text text-padding-10px">Đường phố</div>
             <div class="ms-form-text"></div>
           </div>
         </div>
       </div>
-      <div class="ms-dialog-footer"></div>
+
+      <!-- dialog-footer -->
+      <div class="ms-dialog-footer">
+        <div class="ms-footer-left">
+          <div class="ms-help">
+            <div class="ms-help-icon"></div>
+            <div class="ms-help-text">Trợ giúp</div>
+          </div>
+        </div>
+        <div class="ms-footer-right">
+          <div class="ms-btn-save ms-footer-btn-common">
+            <div class="ms-icon-common ms-btn-save-icon"></div>
+            <div class="ms-btn-save-text">Lưu</div>
+          </div>
+          <div class="ms-btn-build ms-footer-btn-common">
+            <div class="ms-btn-build-icon ms-icon-common"></div>
+            <div class="ms-btn-build-text">Lưu và thêm mới</div>
+          </div>
+          <div class="ms-btn-cancel ms-footer-btn-common" @click="closeDialog">
+            <div class="ms-btn-cancel-icon ms-icon-common"></div>
+            <div class="ms-btn-cancel-text">Hủy bỏ</div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 <script>
-export default {}
+export default {
+  name: 'msDialog',
+  data () {
+    return {
+      store: {
+        storeId: '00000000-0000-0000-0000-000000000000',
+        storeCode: '',
+        storeName: '',
+        address: '',
+        phoneNumber: '',
+        storeTaxCode: '',
+        provinceId: '00000000-0000-0000-0000-000000000000',
+        districtId: '00000000-0000-0000-0000-000000000000',
+        wardId: '00000000-0000-0000-0000-000000000000',
+        street: '',
+        status: 0,
+        createdDate: '2017-08-15T13:32:46',
+        createdBy: '',
+        modifiedDate: '1970-01-01T01:45:10',
+        modifiedBy: '',
+        editMode: 0
+      },
+      listCounry: [],
+      listDistrict: [],
+      listProvince: [],
+      listWard: []
+    }
+  },
+  methods: {
+    closeDialog () {
+      this.$emit('closeDialog')
+    },
+    loadCountry(countryId){
+this.
+    }
+  }
+}
 </script>
 <style lang="scss" scoped>
 @import "../../scss/common.scss";
--- src/layout/msContent/msContent.vue
+++ src/layout/msContent/msContent.vue
@@ -1,16 +1,34 @@
 <template>
     <div class="ms-content">
-        <ms-tool-bar />
+        <ms-tool-bar  @openDialog='openDialog'/>
         <ms-grid />
-        <ms-dialog />
+        <ms-dialog v-show="isActiveDialog" @closeDialog='isActiveDialog = false' ref="dialog"/>
+        <ms-popup-delete v-show="isActivePopupDelete" />
+        <ms-popup-save v-show="isActivePopupSave" />
     </div>
 </template>
 <script>
-
+import msPopupSave from '../../components/msPopupSave/msPopupSave.vue'
 export default {
-  name: 'msContent'
+  components: { msPopupSave },
+  name: 'msContent',
+  data () {
+    return {
+      isActiveDialog: false, // Biến hiển thị dialog
+      isActivePopupDelete: false, // Biến hiển thị popup delete
+      isActivePopupSave: false // Biến hiển thị popup delete
+    }
+  },
+  methods:
+  {
+    // Hàm hiển thị dialog thêm mới
+    openDialog () {
+      this.isActiveDialog = true
+    }
+  }
 }
 </script>
+
 <style lang="scss">
 @import '../../scss/common.scss';
 @import '../../scss/msContent.scss'
